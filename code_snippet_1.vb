80fcb42b05f5785821f7c35fd50b64039792e8eb
radixy0 <80622923+radixy0@users.noreply.github.com>
2021-04-18 19:54:53 +0200

    Create code_snippet_1.vb

---
+++ code_snippet_1.vb
@@ -0,0 +1,34 @@
+        Dim newWebKey As String = "CHANGED@CHANGED" 'Placeholder, get new web key from text field!
+
+        Dim pattern As Regex = New Regex(".*\[(.*webkey"":"")(.*)("",""lineItemId.*)\].*")
+        Dim match As Match = pattern.Match(input)
+        Dim toSend As String = ""
+        If (match.Success) Then
+            toSend = match.Groups(1).ToString() + newWebKey + match.Groups(3).ToString()
+        Else
+            'Throw Error: Regular Expression did not match input
+        End If
+
+
+        'Convert object to bytes
+        Dim jsondata As Byte() = Encoding.UTF8.GetBytes(toSend)
+
+        'set up webrequest
+        Dim request As WebRequest
+        request = WebRequest.Create("https://go.industrysoftware.automation.siemens.com/c2o/assignment/v2/assign")
+        request.Method = "POST"
+        request.ContentLength = jsondata.Length
+        request.ContentType = "application/json"
+
+        'send data
+        Dim requestStream As Stream = request.GetRequestStream
+        requestStream.Write(jsondata, 0, jsondata.Length)
+        requestStream.Close()
+
+        'receive answer
+        Dim responseStream As Stream = request.GetResponse.GetResponseStream
+        Dim responseReader As New StreamReader(responseStream)
+        Dim response As String = responseReader.ReadToEnd
+
+        'Do stuff with response
+        'Console.WriteLine("response: " + response)

2025396d8d1e4bd6fe3bc1b89039e8a16599492d
radixy0 <80622923+radixy0@users.noreply.github.com>
2021-04-18 21:10:13 +0200

    Update code_snippet_1.vb

--- code_snippet_1.vb
+++ code_snippet_1.vb
@@ -1,22 +1,21 @@
         Dim newWebKey As String = "CHANGED@CHANGED" 'Placeholder, get new web key from text field!
-        
-        'change webkey in input (from list) using regex
-        Dim pattern As Regex = New Regex(".*\[(.*webkey"":"")(.*)("",""lineItemId.*)\].*")
+
+        Dim pattern As Regex = New Regex(".*\[(.*webkey"":)(.*)(,""lineItemId.*)\].*")
         Dim match As Match = pattern.Match(input)
         Dim toSend As String = ""
         If (match.Success) Then
-            toSend = match.Groups(1).ToString() + newWebKey + match.Groups(3).ToString()
+            toSend = match.Groups(1).ToString() + """" + newWebKey + """" + match.Groups(3).ToString()
         Else
             'Throw Error: Regular Expression did not match input
         End If
 
 
-        'Convert toSend to bytes
+        'Convert object to bytes
         Dim jsondata As Byte() = Encoding.UTF8.GetBytes(toSend)
 
         'set up webrequest
         Dim request As WebRequest
-        request = WebRequest.Create("https://go.industrysoftware.automation.siemens.com/c2o/assignment/v2/assign")
+        request = WebRequest.Create("http://localhost:8080/echo") 'https://go.industrysoftware.automation.siemens.com/c2o/assignment/v2/assign")
         request.Method = "POST"
         request.ContentLength = jsondata.Length
         request.ContentType = "application/json"
